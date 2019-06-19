82bcb81157f959058d94a0f17adaaae0dc04bcef
Tigran# <Tigran#@LAPTOP-0CV90VBK>
2019-06-19 19:16:04 +0400

    tiko

--- Clientapp/Program.cs
+++ Clientapp/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.ServiceModel;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,6 +11,22 @@ namespace Clientapp
     {
         static void Main(string[] args)
         {
+            Uri _uri = new Uri("http://Localhost:8088/ServiseRelasietion");
+            BasicHttpBinding basic = new BasicHttpBinding();
+            ChannelFactory<IContract> channel = new ChannelFactory<IContract>(basic, new EndpointAddress(_uri));
+            var OpenChannel = channel.CreateChannel();
+            while (true) {
+
+                string text = Console.ReadLine();
+                if (text == "EXIT")
+                { break; }
+                else {
+
+
+                    Console.WriteLine(OpenChannel.gettikotime(text));
+                }
+         
+        }
         }
     }
 }
